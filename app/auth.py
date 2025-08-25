253e3afd8b82b5775d2401c83cea6f7af60dd263
Heron <c.heron.22@outlook.com>
2025-08-25 15:34:08 +0100

    Update footer and add dynamic current year

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from datetime import datetime
 
 # Blueprint for user authentication routes (login/register)
 auth_bp = Blueprint("auth", __name__)
@@ -9,9 +10,9 @@ auth_bp = Blueprint("auth", __name__)
 # Register route
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    return render_template("register.html", current_year = datetime.now().year)
 
 # Log in route
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", current_year = datetime.now().year)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
+from datetime import datetime
 
 # Blueprint for home page
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", current_year = datetime.now().year)
